74c5b134760f2351f6761c60237e1f31b2a73804
Anne Marie Merritt <amm@bluedata.com>
2018-05-23 16:17:58 -0700

    Create a way to change the interval timeout via the commandline.
    This is not the same as creating the default value, which is what
    is done in the bluedata.cfg file at install time.
    
    Signed-off-by: Anne Marie Merritt <amm@bluedata.com>

---
+++ amm/changetimeout.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+VNODE_TIMEOUT=`expr $1`
+
+ERTS_PATH=/opt/bluedata/common-install/bd_mgmt/erts-*/bin
+NAME_ARG=`egrep '^-s?name' $ERTS_PATH/../../releases/1/vm.args`
+RPCCMD="$ERTS_PATH/escript $ERTS_PATH/nodetool $NAME_ARG -setcookie cookie rpcterms"
+$RPCCMD bd_mgmt_config update "session_timeout_interval_hours. $VNODE_TIMEOUT."
+
