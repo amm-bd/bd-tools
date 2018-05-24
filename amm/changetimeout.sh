b8ace26b97e5372c39cc91de682935b1cb227a0f
Anne Marie Merritt <amm@bluedata.com>
2018-05-23 17:53:45 -0700

    Slightly modify expiry variable after a refactoring.
    Signed-off-by: Anne Marie Merritt <amm@bluedata.com>

--- amm/changetimeout.sh
+++ amm/changetimeout.sh
@@ -5,5 +5,4 @@ VNODE_TIMEOUT=`expr $1`
 ERTS_PATH=/opt/bluedata/common-install/bd_mgmt/erts-*/bin
 NAME_ARG=`egrep '^-s?name' $ERTS_PATH/../../releases/1/vm.args`
 RPCCMD="$ERTS_PATH/escript $ERTS_PATH/nodetool $NAME_ARG -setcookie cookie rpcterms"
-$RPCCMD bd_mgmt_config update "session_timeout_interval_hours. $VNODE_TIMEOUT."
-
+$RPCCMD bd_mgmt_config update "session_expiry_hours. $VNODE_TIMEOUT."
