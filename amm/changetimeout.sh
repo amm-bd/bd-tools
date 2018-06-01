e1615c46e35d3708e25f6521d7762d82f8c7ae52
Anne Marie Merritt <amm@bluedata.com>
2018-06-01 13:50:21 -0700

    Add a script to change the session expiry timeout value in hours for an EPIC installation.
    Signed-off-by: Anne Marie Merritt <amm@bluedata.com>

--- amm/changetimeout.sh
+++ amm/changetimeout.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/bash
 
+if [[ $# -ne 1 ]]; then
+    echo "Usage:  Enter session expiry hours timeout value as the only argument."
+    exit 1
+fi
+
 VNODE_TIMEOUT=`expr $1`
 
 ERTS_PATH=/opt/bluedata/common-install/bd_mgmt/erts-*/bin
 NAME_ARG=`egrep '^-s?name' $ERTS_PATH/../../releases/1/vm.args`
 RPCCMD="$ERTS_PATH/escript $ERTS_PATH/nodetool $NAME_ARG -setcookie cookie rpcterms"
-$RPCCMD bd_mgmt_config update "session_expiry_hours. $VNODE_TIMEOUT."
+$RPCCMD bd_mgmt_api_identity set_session_expiry_hours $VNODE_TIMEOUT.
+
