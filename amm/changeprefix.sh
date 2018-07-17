6b5933d7cb2b103df2622fcdcf67c49c18371251
Anne Marie Merritt <amm@bluedata.com>
2018-07-16 17:33:50 -0700

    Enable the lines that actually perform the change.
    Signed-off-by: Anne Marie Merritt <amm@bluedata.com>

--- amm/changeprefix.sh
+++ amm/changeprefix.sh
@@ -34,11 +34,11 @@ else
 fi
 
 
-#bdconfig --set bdshared_global_bdprefix="$VNODE_DOMAIN_PREFIX"
+bdconfig --set bdshared_global_bdprefix="$VNODE_DOMAIN_PREFIX"
 
-#ERTS_PATH=/opt/bluedata/common-install/bd_mgmt/erts-*/bin
-#NAME_ARG=`egrep '^-s?name' $ERTS_PATH/../../releases/1/vm.args`
-#RPCCMD="$ERTS_PATH/escript $ERTS_PATH/nodetool $NAME_ARG -setcookie cookie rpcterms"
-#$RPCCMD bd_mgmt_config update "bdshared_global_bdprefix. \"$VNODE_DOMAIN_PREFIX\"."
+ERTS_PATH=/opt/bluedata/common-install/bd_mgmt/erts-*/bin
+NAME_ARG=`egrep '^-s?name' $ERTS_PATH/../../releases/1/vm.args`
+RPCCMD="$ERTS_PATH/escript $ERTS_PATH/nodetool $NAME_ARG -setcookie cookie rpcterms"
+$RPCCMD bd_mgmt_config update "bdshared_global_bdprefix. \"$VNODE_DOMAIN_PREFIX\"."
 
 
