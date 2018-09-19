276536f2eee00cfcb5652ce6674db264448e5c2e
Anne Marie Merritt <amm@bluedata.com>
2018-09-18 18:08:25 -0700

    Tool to stop and reload the monitoring container without running the start-all script so it can be debugged.
    Signed-off-by: Anne Marie Merritt <amm@bluedata.com>

---
+++ redo.sh
@@ -0,0 +1,7 @@
+# Unload monitoring container and restart it without calling start-all so we can debug it.
+
+export mon=`docker ps | grep monitor | awk '{print $1}'`
+docker stop $mon; docker rm $mon
+docker run -it --name=epic-monitoring-10.32.1.76 --cap-add=IPC_LOCK --net=host --ulimit memlock=-1:-1 --pid=host -c 2048 -m 4GB --memory-swap -1 --volume=/proc:/hostfs/proc:ro --volume=/:/hostfs:ro,rslave --volume=/var/run/docker.sock:/var/run/docker.sock:ro --volume=/var/lib/docker:/var/lib/docker:ro --volume=/var/lib/monitoring:/var/lib/monitoring --ulimit nofile=65536 --restart=always --entrypoint 'bash' epic/monitoring:1.2
+
+
